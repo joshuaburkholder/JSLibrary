4bfde5e9052cd61b27a2745411202c9edb8efd45
Joshua <Joshua>
2017-01-21 17:11:17 -0500

    inital commit

---
+++ 4.02_functions_invocation.js
@@ -0,0 +1,29 @@
+//When a function is invoked, the current function is suspended and the
+//parameters are passed to the new function. 
+
+//In addition to the parameters of a function, every function also recieves
+//"this" and "argument" parameters. The value of "this" parameter is determined
+//by the "invocation pattern"
+
+//There are FOUR INVOCATION PATTERNS in JavaScript:
+//  1. Method Invocation
+//  2. Function Invocation
+//  3. Constructor Invocoation
+//  4. Apply Invocation
+
+//The INVOCATION OPERATOR is (). These () may contain zero (or more!)
+//expressions, separated by comma(s). 
+//Each value of the arguement will be assigned to the function's parameters.
+//If there are too many argument values, the extras will be ignored.
+//If there are too few argument values, the missing values of the parameters
+//will be returned "undefined."
+
+
+/*------------------ METHOD INVOCATION ---------------------*/
+
+
+/*------------------ FUNCTION INVOCATION ---------------------*/
+
+/*------------------ CONSTRUCTOR INVOCATION -----------------*/
+
+/*------------------ APPLY INVOCATION ---------------------*/
