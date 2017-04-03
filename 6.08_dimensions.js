d5b58cd18796f10a0c615b4015b9f23a2a87940d
DESKTOP-BGOGHIS\BurkLesmeisterholder <joshburkholder31@gmail.com>
2017-04-02 23:29:02 -0400

    added dimensions p and intro to objects

--- 6.08_dimensions.js
+++ 6.08_dimensions.js
@@ -1,3 +1,9 @@
+
+
+// ************ Underconstruction******************//
+// **************************************************
+// **************************************************
+
 //JavaScript des not have arrays with more than one dimension, but
 //can have "arrays of arrays".
 
@@ -24,7 +30,34 @@ console.log(matrix);
 //creating a matrix this way, all the values will be undefined, and
 //if you want a different initial value, you would have to set it explicitly.
 
+Array.matrix = function (m, n, initial) {
+	var a, i, j, mat = [];
+	for (i=0; i < m; i += 1){
+		a = [];
+		for (j =0; j < n; j += 1){
+			a[j] = initial;
+		}
+	mat[i] =a;
+	}
+  return mat;
+}
 
 //Make a 4*4 array
 
+var myMatrix = Array.matrix(4, 4, 0);
+console.log(myMatrix[3][3]);
+
+
 //make an identity array
+
+Array.identity = function (n) {
+	var i, mat = Array.matrix(n, n, 0);
+	for (i = 0; i < n; i += 1) {
+		mat[i][i] = 1;
+	}
+  return mat;
+};
+
+myMatrix = Array.identity(4);
+
+console.log(myMatrix[3][3]); //1
