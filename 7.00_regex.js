f86df4c9e28ff29a9f0a234c987f57a535db53b2
DESKTOP-BGOGHIS\BurkLesmeisterholder <joshburkholder31@gmail.com>
2017-07-27 00:17:33 -0400

    added regex file

---
+++ 7.00_regex.js
@@ -0,0 +1,13 @@
+//REGULAR EXPRESSIONS
+
+// Regular expression is the syntax of a simple language.
+ // JavaScript was borrowed from Java & Perl. 
+
+// RegEx are used with methods to SEARCH, REPLACE, and EXTRACT information from strings.
+
+// Characters are sometimes interpreted as literals or operators, depending on the context.
+
+// ---------------------------------------
+
+
+^  //Indicates the BEGINNING OF A STRING
