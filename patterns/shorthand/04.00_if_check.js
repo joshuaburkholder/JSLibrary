072028d5900f9c09fa82d4d43f632050e48056b5
DESKTOP-BGOGHIS\BurkLesmeisterholder <joshburkholder31@gmail.com>
2017-08-19 02:47:59 -0400

    'update'

---
+++ patterns/shorthand/04.00_if_check.js
@@ -0,0 +1,25 @@
+//When using if statements to check values, you can omit assigning a boolean condition
+
+//longhand
+
+// if(isCold = true){...}
+
+// //shorthand
+
+// if(isCold){...}
+
+//SECOND EXAMPLE
+
+let hot;
+
+//longhand
+
+if(hot !== true){
+	console.log('not true');
+}
+
+//shorthand
+
+if (!hot){
+	console.log('not hot');
+}
