3b3b87ad836c19d9ffc52db59225d46a4482fe7e
DESKTOP-BGOGHIS\BurkLesmeisterholder <joshburkholder31@gmail.com>
2017-08-20 00:04:13 -0400

    'add'

---
+++ patterns/shorthand/11.00_multiple_line_strings.js
@@ -0,0 +1,22 @@
+// Backticks can save you a lot of typing when writing multi-line strings
+
+//Longhand
+
+const hipsterTalk = 'Sustainable seitan pour-over sriracha, \n\t'
+	+ 'franzen man braid small batch celiac. Literally locavore \n\t' 
+	+ 'four loko, food truck migas single-origin coffee lumbersexual \n\t'
+	+ 'banjo sriracha ennui next level mustache iceland 3 wolf moon \n\t'
+	+ 'tilde. Woke intelligentsia jean shorts jianbing four loko, \n\t'
+	+ 'meh brooklyn YOLO unicorn viral schlitz pop-up. \n\t'
+
+//Shorthand
+
+const hipsterChat = `Sustainable seitan pour-over sriracha,
+ franzen man braid small batch celiac. Literally locavore 
+ four loko, food truck migas single-origin coffee lumbersexual 
+ banjo sriracha ennui next level mustache iceland 3 wolf moon 
+ tilde. Woke intelligentsia jean shorts jianbing four loko, 
+ meh brooklyn YOLO unicorn viral schlitz pop-up. `
+
+ console.log(hipsterTalk);
+ console.log(hipsterChat);
