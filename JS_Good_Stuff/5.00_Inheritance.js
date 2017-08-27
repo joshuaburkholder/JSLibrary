c994340b465cdd43fbc5003168ae776e3095f447
DESKTOP-BGOGHIS\BurkLesmeisterholder <joshburkholder31@gmail.com>
2017-08-26 23:52:53 -0400

    restructuring

---
+++ JS_Good_Stuff/5.00_Inheritance.js
@@ -0,0 +1,17 @@
+// **********Not up-to-date***************
+
+Inheritence
+//Discuss inheritance in JS here
+
+Pseudoclassical
+//Discuss the prototypal nature of JS here
+
+********CONTENTS***************
+5.01: Object Specifiers
+5.02: prototypal pattern
+// 	a. differential inheritance
+// 5.03: Functional Inheritence(intro)
+// 	a. example
+// 	b. dealing with super methods
+// 5.04: Composing Objects through Parts
+
